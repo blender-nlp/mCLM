GNN gradient fixes

mCLM/scripts/test_model.py
@@ -13,13 +13,15 @@ import torch
 from mCLM.data.dataloaders import KinaseDataModule
 from mCLM_tokenizer.tokenizer import get_blocks
 
+from mCLM.model.models import mCLM
+
 import subprocess
 
 
 if __name__ == "__main__":
     torch.cuda.empty_cache()
 
-    subprocess.run(["nvidia-smi"])
+    #subprocess.run(["nvidia-smi"])
 
     config = {
         "pretrained_text_model": "michiyasunaga/BioLinkBERT-base",
@@ -48,12 +50,6 @@ if __name__ == "__main__":
     }
 
     parser = argparse.ArgumentParser(description="Biencoder")
-    parser.add_argument(
-        "--pretrained_text_model",
-        default="michiyasunaga/BioLinkBERT-base",
-        type=str,
-        help="Which text encoder to use from HuggingFace",
-    )
     parser.add_argument("--trunc_length", default=512, type=int)
 
     parser.add_argument("--num_warmup_steps", default=1000, type=int)
@@ -81,10 +77,8 @@ if __name__ == "__main__":
     parser.add_argument("--seed", default=42, type=int)
 
     parser.add_argument("--model", default="mCLM", type=str)
-    parser.add_argument("--base_model", default="meta-llama/Llama-3.1-8B-Instruct", type=str)
-    #parser.add_argument(
-    #    "--freeze_text_encoder", type=bool, action=argparse.BooleanOptionalAction
-    #)
+    parser.add_argument("--base_model", default="/home/a-m/cne2/MMLI_projects/LLMs/Llama-3.2-1B-Instruct/", type=str)
+    parser.add_argument("--pretrained_text_model", default="/home/a-m/cne2/MMLI_projects/LLMs/Llama-3.2-1B-Instruct/", type=str)
 
     parser.add_argument(
         "--freeze_GNN", type=bool, action=argparse.BooleanOptionalAction
@@ -153,10 +147,11 @@ if __name__ == "__main__":
     # model loading
     from mCLM.model.llama_based.model import LlamaForCausalLM
     ckpt_path = config["pretrained_text_model"]
-    model = LlamaForCausalLM.from_pretrained(ckpt_path)
+    #model = LlamaForCausalLM.from_pretrained(ckpt_path)
+    model = mCLM(config)
 
-    model.extend_text_vocab_size(len(dm.tokenizer.vocab))
-    model.set_mol_vocab(block_ID_to_data)
+    model.model.extend_text_vocab_size(len(dm.tokenizer.vocab))
+    model.model.set_mol_vocab(block_ID_to_data)
     #print(model.config)
 
     # test graph forwarding
@@ -171,17 +166,50 @@ if __name__ == "__main__":
     #print(item["input"]["input_ids"].shape, item["input"]["attention_mask"].shape,
     #    item["input"]["input_ids"].shape)
     model.train(False)
-    model.post_training()
-    inference_output = model(
-        input_ids=item["input"]["input_ids"],
-        attention_mask=item["input"]["attention_mask"],
+    model.model.post_training()
+    inference_output = model.compute_step(
+        item,
+        'test',
+    )
+        #input_ids=item["input"]["input_ids"],
+        #attention_mask=item["input"]["attention_mask"],
         #labels=item["input"]["input_ids"]
-    )
 
-    # model forwarding, training mode
-    model.train(True)
-    training_output = model(
-        input_ids=item["input"]["input_ids"],
-        attention_mask=item["input"]["attention_mask"],
-        labels=item["input"]["labels"]
-    )
+optimizer = torch.optim.Adam(
+    model.parameters(),
+    lr=config['lr'],
+    weight_decay=config['weight_decay'],
+)
+
+# model forwarding, training mode
+model.train(True)
+training_output = model.compute_step(
+    item,
+    'train',
+)
+    #input_ids=item["input"]["input_ids"],
+    #attention_mask=item["input"]["attention_mask"],
+    #labels=item["input"]["labels"]
+training_output['loss'].backward()
+
+for p in model.named_parameters():
+    print(p[0], p[1].requires_grad)
+
+grad_dict = {k:v.grad for k, v in zip(model.state_dict(), model.parameters())}
+print(grad_dict['model.base_model.model.model.mol_gnn.convs.1.nn.0.bias'])
+print(grad_dict['model.base_model.model.lm_head.weight'])
+
+optimizer.step()
+
+orig_model = mCLM(config)
+orig_model.model.extend_text_vocab_size(len(dm.tokenizer.vocab))
+orig_model.model.set_mol_vocab(block_ID_to_data)
+
+key = 'model.base_model.model.model.mol_gnn.convs.1.nn.0.bias'
+print(key, (model.state_dict()[key] == orig_model.state_dict()[key]).all())
+key = 'model.base_model.model.lm_head.weight'
+print(key, (model.state_dict()[key] == orig_model.state_dict()[key]).all())
+key = 'model.base_model.model.model.embed_tokens.weight'
+print(key, (model.state_dict()[key] == orig_model.state_dict()[key]).all())
+key = 'model.base_model.model.model.layers.15.mlp.up_proj.lora_A.default.weight'
+print(key, (model.state_dict()[key] == orig_model.state_dict()[key]).all())